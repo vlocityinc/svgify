313f530d3ffc15ba7e3f3d6cfe38aec601906f2f
Matt Goldspink <mattgoldspink1@gmail.com>
2016-09-08 14:32:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,11 @@ module.exports = function(file) {
   var svg = tr.select('svg')
     .createReadStream()
     .once('data', function() {
-      out.queue('function format(text) {')
-      out.queue(  'return function(x, y) {')
-      out.queue(    'x = (+x|0);')
-      out.queue(    'y = (+y|0);')
-      out.queue(    'var el = document.createElement(\"div\");')
-      out.queue(    'el.innerHTML = "<svg><g><g>" + text + "</g></g></svg>";')
-      out.queue(    'el = el.childNodes[0].childNodes[0];')
-      out.queue(    'el.childNodes[0].setAttribute("transform", "translate(" + x + "," + y + ")");')
-      out.queue(    'return el')
-      out.queue(  '}')
-      out.queue('}\n')
-      out.queue('module.exports = format("')
+      out.queue('module.exports = ')
     })
 
   svg.once('end', function() {
-    out.queue('")')
+    out.queue(';')
     out.queue(null)
   }).on('data', function(data) {
     data = String(data)
